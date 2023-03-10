fac489618a539db64b634b2b27fb68b261d7ec24
Giovanna Sousa <giovannasousa.code@gmail.com>
2023-03-10 16:37:18 -0400

    Implementando a conexão do Firebase ao projeto

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+import 'package:takeanote_app/styles/app_style.dart';
+
+import '../../widgets/lottie_animation.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +16,92 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: AppStyle.mainColor,
+      appBar: AppBar(
+        backgroundColor: AppStyle.mainColor,
+        title: Text(
+          'Take a Note',
+          style: GoogleFonts.nunito(
+            color: Colors.white,
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Suas notas recentes",
+              style: GoogleFonts.roboto(
+                color: Colors.white,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection("Notes").snapshots(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Container(
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.only(top: 250),
+                    color: Colors.amber,
+                    child: Lottie.asset(
+                      "assets/animation/loading_animation.json",
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.fill,
+                    ),
+                  );
+                }
+
+                if (snapshot.hasData) {
+                  return GridView(
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                  );
+                }
+
+                return SizedBox(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const LottieAnimation(
+                          pathAnimation:
+                              "assets/animation/empty_animation.json",
+                          width: 250,
+                          height: 250),
+                      Text(
+                        "Você não possui notas",
+                        style: GoogleFonts.nunito(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
